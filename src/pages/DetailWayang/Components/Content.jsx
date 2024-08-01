69cc573e1e60b4778f0538b59092da3dc781c718
dilarangoding <dilarangoding@proton.me>
2024-08-01 14:21:47 +0700

    add refrence in detail wayang

--- src/pages/DetailWayang/Components/Content.jsx
+++ src/pages/DetailWayang/Components/Content.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Content() {
 
@@ -29,6 +30,7 @@ export default function Content() {
                 alt=""
                 className="img card-img max-w-full md:max-w-[384px] mb-6 rounded-xl py-[25px] px-[49px] shadow-[0_3px_16px_rgba(0,0,0,0.3)]"
               />
+              <p className="flex flex-col items-center -mt-4"><Link to='https://www.vectorstock.com/royalty-free-vectors/wayang-vectors'>sumber : arkyoswara</Link></p>
               <h2 className="flex flex-col items-center"> {puppet.name}</h2>
             </div>
           </div>
